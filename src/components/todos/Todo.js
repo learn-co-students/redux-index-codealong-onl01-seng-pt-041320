8121fa96f9b2a02adb140356e3b8c6f71d91dac2
chica25 <patricia@patriciaco.com>
2021-01-23 22:15:48 -0500

    Done.

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Todos = props => <li>{props.text}</li>
+
+export default Todos;
--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { createStore } from 'redux';
 
 let store = createStore(manageTodo);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export function render(){
+  ReactDOM.render(
+    <Provider>
+      <App store={store} />
+    </Provider>,
+    document.getElementById('root')   
+  );
+}
